e5bca5d87b99f691fb054f27beda85aaaab617c9
tim <tdresser@gmail.com>
2011-06-02 20:23:31 -0400

    trying to clean up ray/sphere intersection

--- tracer.hs
+++ tracer.hs
@@ -1,11 +1,12 @@
 import qualified Data.ByteString as B
+import Data.Maybe
 import Tracertypes
 import Tga
 
 red :: Int -> B.ByteString
 red n = B.pack $ foldr (++) [] $ take n $ repeat $ getAsByteArray (Color 1 0 0)
 
-camera = Camera (Vector 0 0 0) (Quaternion 1 (Vector 0 0 0))
+camera = Camera (Vector 0 0 0) $ qnorm $ Quaternion 1 $ Vector 0.2 0.1 3
 width = 100
 height = 100
 dist = 100
@@ -42,7 +43,11 @@ vdot (Vector x1 y1 z1) (Vector x2 y2 z2) =
   x1 * x2 + y1 * y2 + z1 * z2
 
 vminus (Vector x1 y1 z1) (Vector x2 y2 z2) = 
-  Vector (x1 - x2)  (y1 - y2) (z1 * z2)
+  Vector (x1 - x2)  (y1 - y2) (z1 - z2)
+  
+vplus (Vector x1 y1 z1) (Vector x2 y2 z2) = 
+  Vector (x1 + x2)  (y1 + y2) (z1 + z2)
+
 
 rotate v q =
   (vec 
@@ -64,17 +69,39 @@ rays = [getRay camera (x,y) |
     fwidth = fromIntegral width
     fheight = fromIntegral height
 
+
 s = Sphere (Vector 0 0 101) 30
+light = Vector 100 100 100 
 
 raysphere (Ray o l) (Sphere c r) =
-  vdot l c * vdot l c - vdot c c + r * r >= 0
+  if s < 0 then
+     Nothing
+  else
+    let root = sqrt s
+        nonroot = vdot l c
+        d1 = nonroot - root
+        d2 = nonroot + root in
+    if d1 > 0 then
+      Just $ vtimes l d1
+    else
+      if d2 > 0 then
+        Just $ vtimes l d2
+      else
+        Nothing
+          where s = vdot l c * vdot l c - vdot c c + r * r
 
+shadePointOnObject v = 
+  if isJust $ raysphere (Ray light v) s then
+    Color 1 0 0
+  else
+    Color 0.5 0 0   
 
 raycolor r s = 
-  if raysphere r s then
-    getAsByteArray (Color 1 0 0)
+  if isJust $ i then
+    getAsByteArray $ shadePointOnObject $ fromMaybe (Vector 0 0 0 ) i
   else
     getAsByteArray (Color 0 0 0)
+      where i = raysphere r s
 
 colors = map (\r -> raycolor r s) rays
 image = B.pack $ foldr (++) [] $ colors
